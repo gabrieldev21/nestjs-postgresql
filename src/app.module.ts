0deb1f7e955200c91ae2a9237fca9b7595716672
gabrieldev21 <gabrielp.amorim@live.com>
2024-08-13 10:39:35 -0300

    config: initial PostgresConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 import { UserModule } from './modules/user/user.module';
+import { PostgresConfigService } from './config/postgres.config.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+  ],
 })
 export class AppModule {}
