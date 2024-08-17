b2b712ff200467a4623701caf1c5a1e048dc5fe5
gabrieldev21 <gabrielp.amorim@live.com>
2024-08-17 15:37:32 -0300

    fix: add entities in OrderModule

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -5,9 +5,10 @@ import { UserEntity } from '../user/entities/user.entity';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { OrderEntity } from './entities/order.entity';
+import { ProductEntity } from '../product/entities/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([OrderEntity, UserEntity])],
+  imports: [TypeOrmModule.forFeature([OrderEntity, UserEntity, ProductEntity])],
   controllers: [OrderController],
   providers: [OrderService],
 })
