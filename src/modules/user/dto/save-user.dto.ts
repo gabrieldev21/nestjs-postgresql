4941acbd9cc85438207dfa82d0e55efc21c277ef
gabrieldev21 <gabrielp.amorim@live.com>
2024-08-20 15:58:08 -0300

    refactor: implement password matches in saveUserDto

--- src/modules/user/dto/save-user.dto.ts
+++ src/modules/user/dto/save-user.dto.ts
@@ -1,4 +1,10 @@
-import { IsNotEmpty, IsEmail, MinLength } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsEmail,
+  MinLength,
+  Matches,
+  IsString,
+} from 'class-validator';
 
 import { IsEmailUnique } from 'src/utils/validator/isEmailUnique.validator';
 
@@ -10,6 +16,11 @@ export class SaveUserDto {
   @IsEmailUnique({ message: 'Este email já está em uso.' })
   email: string;
 
+  @IsString()
   @MinLength(6)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/, {
+    message:
+      'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.',
+  })
   password: string;
 }
