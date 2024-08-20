cc6ad8cc5d9ca6d0e10fe4df6e60393333d5bae0
gabrieldev21 <gabrielp.amorim@live.com>
2024-08-20 16:19:36 -0300

    refactor: improve matches in password saveUserDto

--- src/modules/user/dto/save-user.dto.ts
+++ src/modules/user/dto/save-user.dto.ts
@@ -18,9 +18,12 @@ export class SaveUserDto {
 
   @IsString()
   @MinLength(6)
-  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/, {
-    message:
-      'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.',
-  })
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+    {
+      message:
+        'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.',
+    },
+  )
   password: string;
 }
